2ae95d8536211274f7653d268772e48034a6cd17
jmkongas <aaronkongsebastian@gmail.com>
2017-03-03 18:47:04 +0800

    update

---
+++ PhaserFriday/app.js
@@ -0,0 +1,17 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+	game.load.image('sky',"assets/sky.png");
+	game.load.image('star',"assets/star.png");
+	game.load.image('ground',"assets/platform.png");
+	game.load.spritesheet('dude',"assets/dude.png",32,48);
+	game.load.spritesheet('baddie',"assets/baddie.png",32,32);
+}
+
+function create() {
+	//add star to the canvas
+	game.add.sprite(0,0,'star');
+}
+
+function update() {
+}
